1de05f5d908b30727f5c8fb00fc1dcf7913976ee
守夜冠军 <153844178+825906362@users.noreply.github.com>
2025-05-08 23:58:26 +0800

    删除了SSM2

--- SSMfirst(2)/src/main/java/nynu/mapper/CardMapper.java
+++
@@ -1,9 +0,0 @@
-package nynu.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import nynu.pojo.Card;
-import org.apache.ibatis.annotations.Mapper;
-@Mapper
-public interface CardMapper extends BaseMapper<Card> {
-
-}
